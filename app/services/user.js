d2d9ec72fb6be3414b4bfc5d8b9c8d27e051ff57
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2024-02-05 20:24:29 +0700

    add package edit dan delete

--- app/services/user.js
+++ app/services/user.js
@@ -1,31 +1,31 @@
+const { v4: uuidv4 } = require('uuid');
 const { User } = require('../../database/models');
 
-
-async function createUser({username, email, password }) {
+async function createUser({ username, email, password }) {
   try {
+    if (!username || !email || !password) {
+      throw new Error('Username, email, and password are required.');
+    }
+
     const newUser = await User.create({
-        username,
-        email,
-        password
+      id: uuidv4(),
+      username,
+      email,
+      password,
     });
 
     return {
-        success: true,
-        data: newUser
-    }
+      success: true,
+      data: newUser,
+    };
   } catch (error) {
     return {
-        success: false,
-        error
-    }
+      success: false,
+      error: error.message,
+    };
   }
 }
 
-
-
-
-
 module.exports = {
-    createUser,
- 
-}
+  createUser,
+};
